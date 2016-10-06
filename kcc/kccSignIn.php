f6533dc4e83a698cc0c080b312cd0cd5c22ed087
JaeKeoung <tkdlgks@naver.com>
2016-10-06 11:27:35 +0900

    내가 뭘 잘못헀지

--- kcc/kccSignIn.php
+++ kcc/kccSignIn.php
@@ -1,4 +1,14 @@
-﻿<!DOCTYPE html>
+﻿
+ <?php
+    try{
+        $pdo = new PDO('mysql:host=localhost; dbname=kccsignin; charset=utf8', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    } catch (Exception $Exception) {
+        die('접속오류: '.$Exception->getMessage()."<br>");
+    }
+?>
+<!DOCTYPE html>
 <html lang="ko">
 <head>
 <meta charset="utf-8">
@@ -140,15 +150,7 @@
 						</div>			
                                             <form method="post" action="kccSignInView.php" name="kccform">
 						<div class="enter_area">
-                                                    <?php
-                                                        try{
-                                                            $pdo = new PDO('mysql:host=localhost; dbname=kccsignin; charset=utf8', 'root', '');
-                                                            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                                                            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-                                                        } catch (Exception $Exception) {
-                                                            die('접속오류: '.$Exception->getMessage()."<br>");
-                                                        }
-                                                    ?>
+                                                   
                                                         <fieldset>
 							<table class="enter_form">
 							<caption><span>회원정보 입력</span></caption>	
@@ -279,10 +281,55 @@
 							<tr>
 								<th><span>자택 우편번호</span><em>*</em></th>
 								<td>
-                                                                    <input name="HomePost1" type="text" id="h_postcode1" class="inptxt w23">		
-									<span class="hyphen">-</span>
-                                                                    <input name="HomePost2" type="text" id="h_postcode2" class="inptxt w23">
-									<a href="#" class="btn_img btn_cnfrm"><span class="btn_img">우편번호 찾기</span></a>
+                                                                    <input type="text" id="sample6_postcode" placeholder="우편번호">
+                                                                    <input type="button" onclick="sample6_execDaumPostcode()" value="우편번호 찾기"><br>
+                                                                    <input type="text" id="sample6_address" placeholder="주소">
+                                                                    <input type="text" id="sample6_address2" placeholder="상세주소">
+
+                                                                    <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
+                                                                    <script>
+                                                                        function sample6_execDaumPostcode() {
+                                                                        new daum.Postcode({
+                                                                        oncomplete: function(data) {
+                                                                        // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                                                                        // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+                                                                        // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                                                                        var fullAddr = ''; // 최종 주소 변수
+                                                                        var extraAddr = ''; // 조합형 주소 변수
+
+                                                                        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+                                                                        if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                                                                            fullAddr = data.roadAddress;
+
+                                                                        } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                                                                            fullAddr = data.jibunAddress;
+                                                                        }
+
+                                                                        // 사용자가 선택한 주소가 도로명 타입일때 조합한다.
+                                                                        if(data.userSelectedType === 'R'){
+                                                                        //법정동명이 있을 경우 추가한다.
+                                                                        if(data.bname !== ''){
+                                                                            extraAddr += data.bname;
+                                                                        }
+                                                                        // 건물명이 있을 경우 추가한다.
+                                                                        if(data.buildingName !== ''){
+                                                                            extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                                                                        }
+                                                                        // 조합형주소의 유무에 따라 양쪽에 괄호를 추가하여 최종 주소를 만든다.
+                                                                            fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+                                                                        }
+
+                                                                            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                                                                            document.getElementById('sample6_postcode').value = data.zonecode; //5자리 새우편번호 사용
+                                                                            document.getElementById('sample6_address').value = fullAddr;
+
+                                                                            // 커서를 상세주소 필드로 이동한다.
+                                                                            document.getElementById('sample6_address2').focus();
+                                                                        }
+                                                                        }).open();
+                                                                        }
+                                                                        </script>
 								</td>
 							</tr>
 							<tr>
@@ -328,10 +375,10 @@
 							<tr>
 								<th><span>직장 우편번호</span><em>*</em></th>
 								<td>
-                                                                    <input name="Compan yPost1" type="text" id="o_postcode1" class="inptxt w23">		
-									<span class="hyphen">-</span>
-                                                                    <input name="CompanyPost2" type="text" id="o_postcode2" class="inptxt w23">
-									<a href="#" class="btn_img btn_cnfrm"><span class="btn_img">우편번호 찾기</span></a>
+                                                                    <input type="text" id="sample6_postcode" placeholder="우편번호">
+                                                                    <input type="button" onclick="sample6_execDaumPostcode()" value="우편번호 찾기"><br>
+                                                                    <input type="text" id="sample6_address" placeholder="주소">
+                                                                    <input type="text" id="sample6_address2" placeholder="상세주소">
 								</td>
 							</tr>
 							<tr>
@@ -429,14 +476,7 @@
 						</fieldset>
                                                 <div class="btn_wrap">
                                                     <input type="submit" value="취소" style="font-size: 20px;">
-                                                    <?php
-//                                                        $sql = "INSERT INTO kccsignin (KoreanName, EnglishName, BirthDay, ID, Passward, PhoneNumber, CellphoneNumber, SMS, EMail, HomePost1, HomeAdress1, HomeAdress2, DM, CompanyName, JobKind, Position, CompanyPost1, CompanyAdress1, CompanyAdress2, CompanyNumber, FaxNumber, WeddingCheck, WeddingDate) VALUES (:KoreanName, :EnglishName, :BirthDay1+:BirthDay2+:BirthDay3, :ID, :Passward, :PhoneNumber1+:PhoneNumber2+:PhoneNumber3, :CellphoneNumber1+:CellphoneNumber2+:CellphoneNumber3, :SMS, :EMail, :HomePost1+:HomePost2, :HomeAdress1, :HomeAdress2, :DM, :CompanyName, :JobKind, :Position, :CompanyPost1+:CompanyPost2, :CompanyAdress1, :CompanyAdress2, :CompanyNumber1+:CompanyNumber2+:CompanyNumber3, :FaxNumber1+:FaxNumber2+:FaxNumber3, :WeddingCheck, :WeddingDate1+:WeddingDate2+:WeddingDate3, :WeddingDate3)";
-                                                        $KName = 'INSERT INTO kccsignin WHERE KoreanName VALUES "KoreanName"';
-                                                        $prepare = $pdo->prepare($KName);
-                                                            
-                                                        $prepare->bindValue(':KoreanName', 'JavaScript', PDO::PARAM_STR);
-                                                        $prepare->execute();
-                                                    ?>
+                                                   
                                                     <input type="submit" value="다음" style="background-color:#1666b1; color: #ffffff; font-size: 20px;">
                                                 </div>
 						</div>
